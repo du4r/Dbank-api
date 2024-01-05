1d73a177c8ee3a51da4e6967d6e7245c56e69db9
Eduardo Araujo <eh.duuh021@gmail.com>
2024-01-04 21:31:17 -0300

    feat: initial config

---
+++ src/main/java/Dbank/Dbank/DbankApplication.java
@@ -0,0 +1,13 @@
+package Dbank.Dbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Dbank/Dbank/DbankApplicationTests.java
@@ -0,0 +1,13 @@
+package Dbank.Dbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
