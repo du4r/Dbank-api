e2f885c35b8725172a913a5f0ed3c999df0b5754
Eduardo Araujo <eh.duuh021@gmail.com>
2024-01-04 21:32:37 -0300

    feat: create Transaction Entity

---
+++ src/main/java/Dbank/Dbank/domain/transaction/Transaction.java
@@ -0,0 +1,34 @@
+package Dbank.Dbank.domain.transaction;
+
+import Dbank.Dbank.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "transations")
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+
+}
